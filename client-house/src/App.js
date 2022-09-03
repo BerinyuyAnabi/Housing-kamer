c546d363357b856f9df6a59dac5f024e94b4a8dc
ngigordon <ngigordon4@gmail.com>
2022-09-03 10:44:04 +0100

    remove 1 component folder

--- client-house/src/App.js
+++ client-house/src/App.js
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
 import "./App.css";
 import Navigation from "./Components/Navigation";
-=======
-// import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import HomePage from './pages/Home';
->>>>>>> bde3c7641f9c24cc848610145bcbc672f6e13b48
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Navigation />
-=======
-      <HomePage />
->>>>>>> bde3c7641f9c24cc848610145bcbc672f6e13b48
     </div>
   );
 }
--- client-house/src/components/header.js
+++
@@ -1,9 +0,0 @@
-const Header = () =>{
-    return(
-        <>
-            <h1> Here is your Beautiful Header</h1>
-        </>
-    )
-}
-
-export default Header
