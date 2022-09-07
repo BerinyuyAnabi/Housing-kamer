b6ee74781923c5ec2b090a674ae49c451af8384e
ngigordon <ngigordon4@gmail.com>
2022-09-07 16:23:32 +0100

    added all my changes

--- client-house/src/App.js
+++ client-house/src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import Navigation from "./Components/Navigation";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PageNotFound from "./pages/404/PagenotFound";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navigation />
-    </div>
+      <Router>
+        <Routes>
+          <Route path="*" element={<PageNotFound />}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ client-house/src/pages/404/PagenotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PageNotFound() {
+  return (
+    <div className="pagenotfound__container">
+      <h1>404</h1>
+      <p>Page not found</p>
+    </div>
+  );
+}
+
+export default PageNotFound;
